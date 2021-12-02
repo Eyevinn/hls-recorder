5073a3aed4bc6739b999b71ab06783d0292e1105
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2021-12-02 08:26:20 +0100

    rebasing

--- demo.ts
+++ demo.ts
@@ -1,26 +1,28 @@
 import { IRecorderOptions } from "./index.js";
 import { HLSRecorder, ISegments } from "./index";
 
+// With PROGRAM-DATE-TIME live live
+const URI2 =
+  "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8";
+// With MAP vod
+const URI3 = "https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8";
+// With KEY vod
+const URI4 = "https://playertest.longtailvideo.com/adaptive/aes-with-tracks/master.m3u8";
+// With MAP vod all m4s
+const URI5 =
+  "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s-fmp4/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+// With Channel Engine
+const URI6 = "https://demo.vc.eyevinn.technology/channels/eyevinn/master.m3u8";
+// With 24/7 Live Tears Of Steel
+const URI7 = "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8";
+
 const rec_opts: IRecorderOptions = {
   recordDuration: -1,
   windowSize: -1,
   vod: true,
 };
 
-const URI = "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/level_0.m3u8";
-// With PROGRAM-DATE-TIME live live
-const URI2 =
-  "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8";
-// With MAP vod
-const URI3 =
-  "https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8";
-// With KEY vod
-const URI4 =
-  "https://playertest.longtailvideo.com/adaptive/aes-with-tracks/master.m3u8";
-// With MAP vod all m4s
-const URI5 = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s-fmp4/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
-
-const recorder = new HLSRecorder(URI, rec_opts);
+const recorder = new HLSRecorder(URI2, rec_opts);
 
 recorder.on(
   "mseq-increment",
@@ -49,3 +51,4 @@ const run = async () => {
  * Run Driver function
  *********************/
 run();
+
