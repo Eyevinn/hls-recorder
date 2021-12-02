ac9c9b7ccf1c6dd6e520934504805c22199ab2b7
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2021-12-02 15:20:32 +0100

    chore: handle source is playlist

--- demo.ts
+++ demo.ts
@@ -18,13 +18,15 @@ const URI5 =
 const URI6 = "https://demo.vc.eyevinn.technology/channels/eyevinn/master.m3u8";
 // With 24/7 Live Tears Of Steel
 const URI7 = "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8";
+// short-multi stream (demuxed & subtitles) 18 seconds
+const URI8 = "https://lab-live.cdn.eyevinn.technology/ED_V4/master.m3u8"
 
 const rec_opts: IRecorderOptions = {
-  recordDuration: 180,
-  windowSize: -1,
+  recordDuration: -1,
+  windowSize: -1,             // -1 for infinite* (max cap defaults at 30 000 seconds/5 minutes if source is a live manifest. To overwrite the max cap, just specify a windowsize.)
   vod: true,
 };
-const recorder = new HLSRecorder(URI6, rec_opts);
+const recorder = new HLSRecorder(URI8, rec_opts);
 recorder.on("mseq-increment", async (data: { allPlaylistSegments: ISegments }) => {
   console.log(`recorder 1 event! PING`);
 });
